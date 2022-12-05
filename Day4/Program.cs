c93fa29377db9772607053e6f91380019a3fa4c4
Jente Vets <jentevets@gmail.com>
2022-12-05 21:23:09 +0100

    Day 4 step 1

---
+++ Day4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Welcome to the DAY 4!");
+Console.WriteLine("");
+Console.WriteLine("Started reading input...");
+var inputText = await File.ReadAllLinesAsync(@"Input/input.txt");
+
+var totalCompleteOverlaps = inputText
+    .Select(x => x.Split(',').SelectMany(y => y.Split('-').Select(int.Parse)).ToArray())
+    .Select(x => DoSectionsOverlapCompletely(x[0], x[1], x[2], x[3]))
+    .Sum();
+
+Console.WriteLine($"Total complete overlaps: {totalCompleteOverlaps}");
+Console.ReadLine();
+
+static long DoSectionsOverlapCompletely(int elf1Start, int elf1Stop, int elf2Start, int elf2Stop)
+{
+    var elf1Range = Enumerable.Range(elf1Start, elf1Stop - elf1Start + 1);
+    var elf2Range = Enumerable.Range(elf2Start, elf2Stop - elf2Start + 1);
+    var overlap = elf1Range.Intersect(elf2Range);
+    if (Enumerable.SequenceEqual(overlap, elf1Range) || Enumerable.SequenceEqual(overlap, elf2Range))
+        return 1;
+    return 0;
+}
