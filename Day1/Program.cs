7dc803d9120e8dbc6bd87afc425eac7321aaaa22
Jente Vets <jentevets@gmail.com>
2022-12-05 18:24:31 +0100

    Day 1 Step 1

---
+++ Day1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Welcome to the DAY 1!");
+Console.WriteLine("");
+Console.WriteLine("Started reading input...");
+var inputText = await File.ReadAllTextAsync(@"Input/input.txt");
+var combinedCaloriesForHighestCaloriesCarryingElf = inputText
+    .Split("\r\n\r\n")
+    .Select(x =>
+    x.Split("\r\n").Select(long.Parse).Sum())
+    .OrderByDescending(x => x)
+    .First();
+Console.WriteLine($"Most calories for an elf: {combinedCaloriesForHighestCaloriesCarryingElf}");
+Console.ReadLine();
