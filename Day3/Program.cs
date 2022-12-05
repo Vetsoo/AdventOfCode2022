6f4de8b7b0620b41d1519bc82d0455f0ab883221
Jente Vets <jentevets@gmail.com>
2022-12-05 20:32:32 +0100

    Day 3 step 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -9,9 +9,24 @@ var sacksWithCompartments = inputText.Select(x => x.Insert(x.Length / 2, "|").Sp
 var totalSumOfAllDuplicates = sacksWithCompartments.Select(x => SumOfDuplicates(x[0], x[1])).Sum();
 
 Console.WriteLine($"Total sum of priorities of duplicates: {totalSumOfAllDuplicates}");
+
+var totalSumOfBadges = inputText
+    .Select((x, i) => new { Content = x, Indexer = i })
+    .GroupBy(x => x.Indexer / 3)
+    .Select(x => x.ToList())
+    .Select(x => BadgeScoreForGroup(x[0].Content, x[1].Content, x[2].Content))
+    .Sum();
+
+Console.WriteLine($"Total sum of priorities of badges: {totalSumOfBadges}");
+
 Console.ReadLine();
 
 long SumOfDuplicates(string compartment1, string compartment2)
 {
     return compartment1.ToCharArray().Intersect(compartment2.ToCharArray()).Select(x => _letterValues.IndexOf(x) + 1).Sum();
+}
+
+long BadgeScoreForGroup(string elf1, string elf2, string elf3)
+{
+    return elf1.ToCharArray().Intersect(elf2.ToCharArray().Intersect(elf3.ToCharArray())).Select(x => _letterValues.IndexOf(x) + 1).Sum();
 }
