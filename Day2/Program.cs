56fc8e17842c39545ae5274b4a256a9e33a2da37
Jente Vets <jentevets@gmail.com>
2022-12-05 19:02:06 +0100

    Day 2 step 1

---
+++ Day2/Program.cs
@@ -0,0 +1,45 @@
+﻿// A & X = Rock = 1
+// B & Y = Paper = 2
+// C & Z = Scissors = 3
+// Win = 6
+// Draw = 3
+// Lose = 0
+
+Console.WriteLine("Welcome to the DAY 2!");
+Console.WriteLine("");
+Console.WriteLine("Started reading input...");
+var inputText = await File.ReadAllLinesAsync(@"Input/input.txt");
+
+var totalPointsForMoves = inputText.Select(x => ScoreForMove(x.Split(' ')[1])).Sum();
+var totalPointsForGames = inputText.Select(x => ScoreForGame(x.Split(' ')[0], x.Split(' ')[1])).Sum();
+
+Console.WriteLine($"Total points: {totalPointsForMoves + totalPointsForGames}");
+Console.ReadLine();
+
+long ScoreForMove(string move)
+{
+    switch (move)
+    {
+        case "X": return 1;
+        case "Y": return 2;
+        case "Z": return 3;
+        default: return 0;
+    }
+}
+
+long ScoreForGame(string opponentMove, string move)
+{
+    switch (opponentMove, move)
+    {
+        case ("A", "X"): return 3;
+        case ("A", "Y"): return 6;
+        case ("A", "Z"): return 0;
+        case ("B", "X"): return 0;
+        case ("B", "Y"): return 3;
+        case ("B", "Z"): return 6;
+        case ("C", "X"): return 6;
+        case ("C", "Y"): return 0;
+        case ("C", "Z"): return 3;
+        default: return 0;
+    }
+}
