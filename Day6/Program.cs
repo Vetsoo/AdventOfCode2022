b7f9421963a3f1fd97b73a29635c3c5d4d154f14
Jente Vets <jentevets@gmail.com>
2022-12-06 10:46:03 +0100

    Day 6 step 2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("");
 Console.WriteLine("Started reading input...");
 var inputText = await File.ReadAllLinesAsync(@"Input/input.txt");
 
-var positionOfMarker = inputText
+var positionOfStartOfPacketMarker = inputText
     .Select(x => x.ToCharArray())
     .Select(x =>
     {
@@ -21,7 +21,30 @@ var positionOfMarker = inputText
         return 0;
     });
 
-foreach (var position in positionOfMarker)
+var positionOfStartOfMessageMarker = inputText
+    .Select(x => x.ToCharArray())
+    .Select(x =>
+    {
+        for (int j = 13; j < x.Length; j++)
+        {
+            var previousArray = new char[14];
+            Array.Copy(x, j - 13, previousArray, 0, 14);
+            var duplicate = previousArray.GroupBy(x => x).Any(g => g.Count() > 1);
+
+            if (!duplicate)
+            {
+                return j + 1;
+            }
+        }
+
+        return 0;
+    });
+
+foreach (var position in positionOfStartOfPacketMarker)
+{
+    Console.WriteLine($"Position of marker: {position}");
+}
+foreach (var position in positionOfStartOfMessageMarker)
 {
     Console.WriteLine($"Position of marker: {position}");
 }
