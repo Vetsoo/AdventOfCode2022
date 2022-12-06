f81d3dbf0bde896c8890d58369c95b6ab074874c
Jente Vets <jentevets@gmail.com>
2022-12-06 10:28:26 +0100

    Day 6 step 1

---
+++ Day6/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Welcome to the DAY 6!");
+Console.WriteLine("");
+Console.WriteLine("Started reading input...");
+var inputText = await File.ReadAllLinesAsync(@"Input/input.txt");
+
+var positionOfMarker = inputText
+    .Select(x => x.ToCharArray())
+    .Select(x =>
+    {
+        for (int j = 3; j < x.Length; j++)
+        {
+            var previousArray = new char[4] { x[j - 3], x[j - 2], x[j - 1], x[j] };
+            var duplicate = previousArray.GroupBy(x => x).Any(g => g.Count() > 1);
+
+            if (!duplicate)
+            {
+                return j + 1;
+            }
+        }
+
+        return 0;
+    });
+
+foreach (var position in positionOfMarker)
+{
+    Console.WriteLine($"Position of marker: {position}");
+}
+Console.ReadLine();
