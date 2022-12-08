0cf2c27addeeffcb0d95948407a31b990d75d68a
Jente Vets <jentevets@gmail.com>
2022-12-08 21:28:59 +0100

    Day 8 step 2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -46,5 +46,42 @@ for (var row = 0; row < inputText.Length; row++)
     }
 }
 
+
+var highestScenicScore = 0;
+for (var row = 0; row < inputText.Length; row++)
+{
+    for (var col = 0; col < inputText.First().Length; col++)
+    {
+        var scoreUp = 0;
+        var scoreDown = 0;
+        var scoreLeft = 0;
+        var scoreRight = 0;
+
+        for (var upwards = row - 1; upwards >= 0; upwards--)
+        {
+            scoreUp++;
+            if (forestMap[upwards, col] >= forestMap[row, col]) break;
+        }
+        for (var downwards = row + 1; downwards < inputText.Length; downwards++)
+        {
+            scoreDown++;
+            if (forestMap[scoreDown, col] >= forestMap[row, col]) break;
+        }
+        for (var left = col - 1; left >= 0; left--)
+        {
+            scoreLeft++; if (forestMap[row, left] >= forestMap[row, col]) break;
+        }
+        for (var right = col + 1; right < inputText.First().Length; right++)
+        {
+            scoreRight++; if (forestMap[row, right] >= forestMap[row, col]) break;
+        }
+
+        var newScenicScore = scoreUp * scoreDown * scoreLeft * scoreRight;
+        if (newScenicScore > highestScenicScore)
+            highestScenicScore = newScenicScore;
+    }
+}
+
 Console.WriteLine($"Amount of visible trees: {visibleTrees.Distinct().Count()}");
+Console.WriteLine($"Highest scenic score: {highestScenicScore}");
 Console.ReadLine();
